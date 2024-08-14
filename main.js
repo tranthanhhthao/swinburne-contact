a298d3f03d816eaaf48ffe0bca6674a636c844f6
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2024-08-14 22:36:11 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-// copyText
+// intro
+const introTl = gsap.timeline({defaults: {duration: 1, ease: 'power2.inOut'}});
 
+introTl
+  .to('.content', {
+    opacity: 1,
+    duration: 3,
+    rotate: 0,
+    transformOrigin: 'top center',
+    ease: 'elastic.out(1, 0.3)' // Elastic ease with amplitude 1 and period 0.3
+
+  })
+  .to('.content img', {
+    // rotate: 360,
+    duration: 3,
+  }, '<')
+
+// img click
+const img = document.querySelector('.content img');
+
+img.addEventListener('click', () => {
+  gsap.to(img, {
+    rotate: '+=360',
+  })
+})
+  
+// copyText
 const button = document.getElementById('copy-address');
 
 function copyText() {
