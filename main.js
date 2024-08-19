6ad33a9eedbe8b8e2f95fefe30c5d6e42e594a2b
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2024-08-19 14:49:17 +0700

    new way of circles

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.defaults({
-  toggleActions: 'restart pause restart pause',
-  scroller: '.content'
+  toggleActions: 'restart reverse restart reverse',
+  scroller: '.content',
+  start: '-10% top',
+  end: () => `+=${document.querySelector('.content').offsetHeight}`,
+  // markers: true,
+  
 });
 
-gsap.set('.first h2', {
+gsap.set(['.first h2', '.first img'], {
   autoAlpha: 1,
 })
 
@@ -16,6 +20,7 @@ gsap.from('.first h2', {
   duration: 1,
   y: 100,
 })
+
 
 gsap.from(['.contact-email h2', '.contact-email .button'], {
   scrollTrigger: '.contact-email',
@@ -27,6 +32,16 @@ gsap.from(['.contact-linktree h2', '.contact-linktree .button'], {
   scrollTrigger: '.contact-linktree',
   duration: 1,
   y: 100,
+})
+
+const circles = gsap.utils.toArray('.circle');
+
+circles.forEach((circle, index) => {
+    gsap.to(circle, {
+      scrollTrigger: `.link:nth-of-type(${index + 1})`,
+      backgroundColor: '#000',
+      duration: .3,
+})
 })
 
 // copyText
@@ -46,48 +61,48 @@ function copyText() {
 button.addEventListener('click', copyText);
 
 // track sections
-const content = document.querySelector('.content');
-const circles = document.querySelectorAll('.circle');
+// const content = document.querySelector('.content');
+// const circles = document.querySelectorAll('.circle');
 
-function getCurrentSection() {
-  const sections = gsap.utils.toArray('.link'); // Get all the sections on the page
-  const currentPosition = content.scrollTop; // Get the current scroll position
+// function getCurrentSection() {
+//   const sections = gsap.utils.toArray('.link'); // Get all the sections on the page
+//   const currentPosition = content.scrollTop; // Get the current scroll position
 
-  let currentSection = null;
+//   let currentSection = null;
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop; // Get the top position of each section
-    const sectionHeight = section.offsetHeight; // Get the height of each section
-    const sectionBottom = sectionTop + sectionHeight; // Calculate the bottom position of each section
-    // console.log(currentPosition, sectionTop + "-" + sectionBottom)
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop; // Get the top position of each section
+//     const sectionHeight = section.offsetHeight; // Get the height of each section
+//     const sectionBottom = sectionTop + sectionHeight; // Calculate the bottom position of each section
+//     // console.log(currentPosition, sectionTop + "-" + sectionBottom)
 
-    if (currentPosition >= sectionTop-1 && currentPosition < sectionBottom) {
-      currentSection = section;
-    }
-  });
+//     if (currentPosition >= sectionTop-1 && currentPosition < sectionBottom) {
+//       currentSection = section;
+//     }
+//   });
 
-  return currentSection;
-}
+//   return currentSection;
+// }
 
-content.addEventListener('scroll', () => {
-  const currentSection = getCurrentSection();
-  // console.log(currentSection); // Print the ID of the current section
+// content.addEventListener('scroll', () => {
+//   const currentSection = getCurrentSection();
+//   // console.log(currentSection); // Print the ID of the current section
 
-  if (currentSection.id == 'first') {
-    circles[0].style.backgroundColor = '#000';
-    circles[1].style.backgroundColor = 'transparent';
-    circles[2].style.backgroundColor = 'transparent';
+//   if (currentSection.id == 'first') {
+//     circles[0].style.backgroundColor = '#000';
+//     circles[1].style.backgroundColor = 'transparent';
+//     circles[2].style.backgroundColor = 'transparent';
 
-  } else if (currentSection.id == 'contact-email') {
-    circles[0].style.backgroundColor = 'transparent';
-    circles[1].style.backgroundColor = '#000';
-    circles[2].style.backgroundColor = 'transparent';
-  } else if (currentSection.id == 'contact-linktree') {
-    circles[0].style.backgroundColor = 'transparent';
-    circles[1].style.backgroundColor = 'transparent';
-    circles[2].style.backgroundColor = '#000';
-  }
-});
+//   } else if (currentSection.id == 'contact-email') {
+//     circles[0].style.backgroundColor = 'transparent';
+//     circles[1].style.backgroundColor = '#000';
+//     circles[2].style.backgroundColor = 'transparent';
+//   } else if (currentSection.id == 'contact-linktree') {
+//     circles[0].style.backgroundColor = 'transparent';
+//     circles[1].style.backgroundColor = 'transparent';
+//     circles[2].style.backgroundColor = '#000';
+//   }
+// });
 
 // preloader
 const buttonProjects = document.getElementById('click-projects');
